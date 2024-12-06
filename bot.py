742541cb87897ff4d0d060acc53e3104673af8bf
Tim Jacob <timothe.jacob@etu.univ-grenoble-alpes.fr>
2024-12-06 11:28:22 +0100

    adding a toggle_text_triggers command

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
 import discord
 from discord import app_commands
+from discord.app_commands import guilds
 from dotenv import load_dotenv
 import os
 import logging
 import re
 from datetime import datetime, timedelta
 import sqlite3
+
+##################### LOGGER #####################
 
 # log directory
 LOG_DIR = "./logs"
@@ -54,6 +57,20 @@ def cleanup_old_logs():
 
 logger = setup_logger()
 
+##################### DISCORD BOT METHODS #####################
+
+# get the token
+load_dotenv("token.env")
+TOKEN = os.getenv("DISCORD_TOKEN")
+if not TOKEN:
+    logger.error("There is no token!")
+    raise ValueError("There is no token!")
+
+# basic bot configuration
+intents = discord.Intents.all()
+bot = discord.Client(intents=intents)
+tree = app_commands.CommandTree(bot)
+
 
 def normalize_emoji(emoji):
     """
@@ -73,17 +90,26 @@ def process_message_for_smiley(message):
     guild_name = message.guild.name if message.guild else "DM"
     user_name = message.author.name
 
-    # break the message into words
-    words = [normalize_emoji(word) for word in message.content.split()]
-    smileys = []
-
     # connect to the database
     conn = sqlite3.connect('databases/bot.db')
     cursor = conn.cursor()
 
+    # import the server settings for this guild
+    cursor.execute("SELECT text_reactions_enabled FROM server_settings WHERE guild_id = ?", (guild_id,))
+    text_reactions_enabled = cursor.fetchone()[0]
+
+
+    # break the message into words
+    words = [normalize_emoji(word) for word in message.content.split()]
+    smileys = []
+
+
     # first : scan for special triggers
     for word in words:
-        cursor.execute("SELECT * FROM special_triggers WHERE word = ?", (word,))
+        if text_reactions_enabled:
+            cursor.execute("SELECT * FROM special_triggers WHERE word = ?", (word,))
+        else:
+            cursor.execute("SELECT * FROM special_triggers WHERE word = ? AND isEmoji = 1", (word,))
         result = cursor.fetchone()
         # smiley is in result[2]
         if result:
@@ -94,7 +120,10 @@ def process_message_for_smiley(message):
 
     # second : scan for regular triggers
     for word in words:
-        cursor.execute("SELECT * FROM trigger_words WHERE word = ?", (word,))
+        if text_reactions_enabled:
+            cursor.execute("SELECT * FROM trigger_words WHERE word = ?", (word,))
+        else:
+            cursor.execute("SELECT * FROM trigger_words WHERE word = ? AND isEmoji = 1", (word,))
         result = cursor.fetchone()
         # smiley is in result[2]
         if result:
@@ -109,24 +138,46 @@ def process_message_for_smiley(message):
 
 
 
+def add_guild_to_db(guild_id):
+    """
+    function that adds a guild to the server settings database
+    """
+    conn = sqlite3.connect('databases/bot.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM server_settings WHERE guild_id = ?", (guild_id,))
+    result = cursor.fetchone()
+    if not result:
+        logger.info(f"Adding guild {guild_id} to the server settings database.")
+        cursor.execute("INSERT INTO server_settings (guild_id) VALUES (?)", (guild_id,))
+        conn.commit()
+        logger.info(f"Guild {guild_id} added to the server settings database.")
+        conn.close()
 
-# get the token
-load_dotenv("token.env")
-TOKEN = os.getenv("DISCORD_TOKEN")
-if not TOKEN:
-    logger.error("There is no token!")
-    raise ValueError("There is no token!")
 
 
-# basic bot configuration
-intents = discord.Intents.all()
-bot = discord.Client(intents=intents)
-tree = app_commands.CommandTree(bot)
+##################### DISCORD BOT EVENTS #####################
 
 @bot.event
 async def on_ready():
-    print(f"Bot loaded and connected as {bot.user} !")
-    logger.info("Bot started!")
+    ## create the server settings database if it don't exist
+    conn = sqlite3.connect('databases/bot.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS server_settings (
+        guild_id TEXT PRIMARY KEY,
+        text_reactions_enabled BOOLEAN DEFAULT 1
+    )
+    """)
+    conn.commit()
+    conn.close()
+
+    ## adding to the database all the guilds the bot is in
+    guild_list = bot.guilds
+    for guild in guild_list:
+        add_guild_to_db(guild.id)
+
+
+    logger.info(f"Bot started and connected as {bot.user} in {len(guild_list)} server!")
     activity = discord.Activity(type=discord.ActivityType.watching, name="free smiley faces !")
     await bot.change_presence(status=discord.Status.online, activity=activity)
     try:
@@ -138,16 +189,12 @@ async def on_ready():
         logger.error(f"Error syncing command tree: {e}")
 
 
-# slash commands
-@tree.command(name="ping", description="A simple ping command")
-async def ping(interaction):
-    await interaction.response.send_message("Pong !")
+@bot.event
+async def on_guild_join(guild):
+    logger.info(f"Bot has joined a new guild: {guild.name} (ID: {guild.id})")
+    add_guild_to_db(guild.id)
 
-@tree.command(name="help", description="A simple help command")
-async def help_command(interaction):
-    await interaction.response.send_message("Never use paid smileys. <:happyFaceSecondary:1313929340974530692>")
 
-# main program
 @bot.event
 async def on_message(message):
     if message.author.bot:
@@ -158,5 +205,60 @@ async def on_message(message):
         smiley_message = " ".join(smileys)
         await message.channel.send(smiley_message)
 
+
+##################### DISCORD SLASH COMMANDS #####################
+
+@tree.command(name="ping", description="A simple ping command")
+async def ping(interaction):
+    await interaction.response.send_message("Pong ! <a:soccer:1313938627104866394>")
+
+
+@tree.command(name="help", description="A simple help command")
+async def help_command(interaction):
+    await interaction.response.send_message("hey there <:yellow:1313941466862587997> \n"
+                                            "i'm a bot that reacts to specific words and paid smileys with free smileys. <:lore:1314281452204068966> \n\n"
+                                            "**List of commands:** \n"
+                                            "- /ping : a simple ping command \n"
+                                            "- /help : this help message \n"
+                                            "- /toggle_text_triggers : toggles on or off the text triggers (like 'hi')\n")
+
+
+@tree.command(name="toggle_text_triggers", description="Toggles on or off the text triggers (like 'hi'")
+async def toggle_text_triggers(interaction):
+
+    # check if the user is an administrator
+    if not interaction.user.guild_permissions.administrator:
+        await interaction.response.send_message(
+            "You do not have permission to use this command. Only administrators can toggle text triggers. <:redAngry:1313876421227057193>",
+            ephemeral=True
+        )
+        return
+
+
+    guild_id = interaction.guild_id
+    conn = sqlite3.connect('databases/bot.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM server_settings WHERE guild_id = ?", (guild_id,))
+    result = cursor.fetchone()
+
+    ## if the guild is not in the database, which is a huge problem, we add it
+    if not result:
+        cursor.execute("INSERT INTO server_settings (guild_id, text_reactions_enabled) VALUES (?, 1)", (guild_id,))
+        conn.commit()
+        await interaction.response.send_message("Text triggers enabled. <:yellow:1313941466862587997>")
+    else:
+        #normal behavior
+        if result[1]:
+            cursor.execute("UPDATE server_settings SET text_reactions_enabled = 0 WHERE guild_id = ?", (guild_id,))
+            conn.commit()
+            await interaction.response.send_message("Text triggers disabled. <a:bigCry:1313925251108835348>")
+        else:
+            cursor.execute("UPDATE server_settings SET text_reactions_enabled = 1 WHERE guild_id = ?", (guild_id,))
+            conn.commit()
+            await interaction.response.send_message("Text triggers enabled. <:yellow:1313941466862587997>")
+        cursor.execute("SELECT * FROM server_settings WHERE guild_id = ?", (guild_id,))
+        result = cursor.fetchone()
+        logger.info(f"Text triggers settings modified for guild {guild_id}. Current status : {result[1]}")
+    conn.close()
 
 bot.run(TOKEN)
